8b26e9c57e5b4d141a3cccee836829f3302de32b
J.D. Welch <jdwelch@puppetlabs.com>
2017-03-12 17:39:03 -0700

    wow it really hated that commit hash idea

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   res.render('index', {
-    version: require('child_process').execSync('git log -1').toString().trim()
+    version: "1"
   });
 });
 
